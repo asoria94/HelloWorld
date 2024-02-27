8993350fca47419cf03a38eb7f25370d2baa157d
asoria94 <aikee.rs94@gmail.com>
2024-02-27 11:31:18 +0000

    first commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#request for input form the user
+#store input into variable called 'name'
+#request for input from the user 
+#store this inout into a variable called 'age'
+#using the print statement, we will print the string ' Hello World!'
+
+name = input('Enter your name here: ')
+print (name) 
+age = input('How old are you?: ')
+print (age)
+print(f'Hello World!')
